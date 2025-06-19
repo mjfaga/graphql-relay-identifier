4a2b7d398248bc91f4a029d4c94e178486b27a41
Mark Faga <mjfaga@gmail.com>
2025-06-18 23:12:24 -0400

    feat: bundle gem graphql_relay_identifier

---
+++ lib/graphql/relay/identifier.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "identifier/version"
+
+module Graphql
+  module Relay
+    module Identifier
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/graphql/relay/identifier_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Graphql::Relay::Identifier do
+  it "has a version number" do
+    expect(Graphql::Relay::Identifier::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
